82dbb3d7aa3b3b592f124b1fefc340eb6de1af82
ozkavosh <augustoslva@hotmail.com>
2023-10-25 11:12:45 -0300

    Update App.tsx with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyles } from "./styles/Global"
+import { theme } from "./styles/Theme"
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
 
