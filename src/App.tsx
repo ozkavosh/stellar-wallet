42b58d7c4fa74a3d375774ce8963cb970cf6557a
ozkavosh <augustoslva@hotmail.com>
2023-10-25 14:51:46 -0300

    Update App.tsx to show Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from "styled-components"
 import { GlobalStyles } from "./styles/Global"
 import { theme } from "./styles/Theme"
+import Home from "./pages/Home"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      {/*TODO Temporary placement, routing goes here*/}
+      <Header />
+      <Home />
     </ThemeProvider>
   )
 }
