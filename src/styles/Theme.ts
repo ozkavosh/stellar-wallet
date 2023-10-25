b03482f80379f9b1c563e0ece7d855e2fd0c58ec
ozkavosh <augustoslva@hotmail.com>
2023-10-25 12:28:32 -0300

    Rename interface DefaultTheme to IDefaultTheme and update its reference

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,6 +1,6 @@
-import { DefaultTheme } from 'styled-components'
+import { IDefaultTheme } from 'styled-components'
 
-export const theme: DefaultTheme = {
+export const theme: IDefaultTheme = {
   colors: {
     primary: '#FAFAFA',
     secondary: '#FFC80A',
--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,7 +1,7 @@
 import 'styled-components'
 
 declare module 'styled-components' {
-  export interface DefaultTheme {
+  export interface IDefaultTheme {
     colors: {
       primary: '#FAFAFA'
       secondary: '#FFC80A'
