1269e4df81dd989801ef0a2b5d7b2a9bc6b9c296
ozkavosh <augustoslva@hotmail.com>
2023-10-25 14:49:32 -0300

    Update Home page styles

--- src/pages/Home/style.tsx
+++ src/pages/Home/style.tsx
@@ -5,3 +5,21 @@ export const Title = styled.h2`
     color: ${props => props.theme.colors.primary};
     margin-bottom: 3rem;
 `;
+
+export const Container = styled.div`
+    display: flex;
+    min-height: calc(100vh - 10rem);
+    padding: 0 5%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    padding: 1em;
+    width: fit-content;
+    height: fit-content;
+    gap: 1em;
+    flex-direction: column;
+`;
