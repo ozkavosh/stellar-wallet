61ebaec6d9fbf41f7454584d1c8d24e125b2e742
ozkavosh <augustoslva@hotmail.com>
2023-10-25 16:59:15 -0300

    Set cypress base url as env variable and add cypress.env.json to .gitignore

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,12 +1,12 @@
 describe("The Home Page", () => {
   it("Should contain a header", () => {
-    cy.visit("/");
+    cy.visit(cy.request(Cypress.env('BASE_URL')) + "/");
 
     cy.get("header").should("exist");
   });
 
   it("Should contain two buttons", () => {
-    cy.visit("/");
+    cy.visit(cy.request(Cypress.env('BASE_URL')) + "/");
 
     const buttons = cy.get("button");
 
