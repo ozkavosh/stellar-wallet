8a2fbb109af5e8d75e2a9548cc27d9775045845c
ozkavosh <augustoslva@hotmail.com>
2023-10-26 17:00:09 -0300

    Remove tests blank spaces

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -9,19 +9,16 @@ describe("The Home Page", () => {
 
   it("Should contain two buttons", () => {
     const buttons = cy.get("button");
-
     buttons.should("have.length", 2);
   });
 
   it("Should open sign in modal when sign in button is clicked", () => {
     cy.get("button").contains("Connect with a secret key").click();
-
     cy.get("h2").contains("Your secret key");
   });
 
   it("Should open sign up modal when sign up button is clicked", () => {
     cy.get("button").contains("Generate key pair for a new account").click();
-
     cy.get("h2").contains("Create a new account");
   });
 });
