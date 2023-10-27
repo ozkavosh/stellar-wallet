90333e534d36feb99e1080ad5499779783411906
ozkavosh <augustoslva@hotmail.com>
2023-10-27 13:59:48 -0300

    Add account context type definitions

---
+++ src/context/types.d.ts
@@ -0,0 +1,15 @@
+interface IAccountState {
+  publicKey: string;
+  secretKey: string;
+}
+
+interface IAccountAction {
+  type: "SET_PUBLIC_KEY" | "SET_SECRET_KEY" | "SET_ACCOUNT" | "LOGIN_WITH_SECRET_KEY";
+  payload: string | IAccountState | ILoginWithSecretKey;
+}
+
+interface IAccountContext {
+  accountState: IAccountState;
+  dispatch: React.Dispatch<IAccountAction>;
+  loginWithSecretKey: (secretKey: string) => boolean;
+}
