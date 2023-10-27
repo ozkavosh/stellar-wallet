82d53d954d83cddf4b90bed4774baf2cc968c200
ozkavosh <augustoslva@hotmail.com>
2023-10-27 14:05:19 -0300

    Add navigation component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
-
-import Home from '../pages/Home'
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import AuthRoute from "./AuthRoute";
+import NoAuthRoute from "./NoAuthRoute";
 
 export const Navigation = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <NoAuthRoute>
+            <Home />
+          </NoAuthRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <AuthRoute>
+            <Dashboard />
+          </AuthRoute>
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
