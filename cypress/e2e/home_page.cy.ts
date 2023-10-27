5bf8071c929a6a0dac387f094d6fdaa810e94561
ozkavosh <augustoslva@hotmail.com>
2023-10-27 14:18:50 -0300

    Update tests with login functionality

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -21,4 +21,27 @@ describe("The Home Page", () => {
     cy.get("button").contains("Generate key pair for a new account").click();
     cy.get("h2").contains("Create a new account");
   });
+
+  it("Should get an error message when input an invalid secret key format", () => {
+    cy.get("button").contains("Connect with a secret key").click();
+    cy.get("input").type("invalid secret key example");
+    cy.get("button").contains("Connect").click();
+    cy.get("p").contains("Invalid secret key. Must start with S and be 56 characters long.");
+  });
+
+  it("Should get an error message when the secret key couldn't be validated with StellarSDK", () => {
+    cy.get("button").contains("Connect with a secret key").click();
+    cy.get("input").type("SCIB7TEDNENDEVW6AYWYQ7IHSFLXCJ67JK4ACZYX6JHMBIKV74OGDBJ7");
+    cy.get("button").contains("Connect").click();
+    cy.get("p").contains("An error has ocurred while login with secret key.");
+  });
+
+  it("Should redirect to dashboard on successful login", () => {
+    cy.get("button").contains("Connect with a secret key").click();
+    cy.get("input").type("SCIB7TEDNENDEVW6AYWYQ7IHSFL5CJ67JK4ACZYX6JHMBIKV74OGDBJ7");
+    cy.get("button").contains("Connect").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/dashboard");
+    });
+  });
 });
