9e6243e4fb2906f71bb39a019c982550f98c49f2
ozkavosh <augustoslva@hotmail.com>
2023-10-27 14:02:00 -0300

    Update SignInModal with onSignIn prop and error handling

--- src/components/Modal/SignInModal/index.tsx
+++ src/components/Modal/SignInModal/index.tsx
@@ -1,20 +1,33 @@
 import { FC, useState } from "react";
 import BaseModal from "../BaseModal";
-import { TextInput } from "../style";
+import { ErrorText, TextInput } from "../style";
 import { Button } from "../../Button";
+import validateSecretKeyInput from "../../../utils/validateSecretKeyInput";
 
 interface IModal {
   showModal: React.SetStateAction<boolean>;
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
-  onSignIn: (secretKey: string) => void;
+  onSignIn: (secretKey: string) => boolean;
 }
 
 const SignInModal: FC<IModal> = ({ showModal, setShowModal, onSignIn }: IModal) => {
   const [secretKey, setSecretKey] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
   const handleTextInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSecretKey(e.target.value);
   };
+
+  const handleButtonClick = () => {
+    if (!validateSecretKeyInput(secretKey)) {
+      const signInTry = onSignIn(secretKey);
+      if (!signInTry) {
+        setError("An error has ocurred while login with secret key.");
+      }
+    }else{
+      setError("Invalid secret key. Must start with S and be 56 characters long.");
+    }
+  }
 
   return (
     <BaseModal showModal={showModal} setShowModal={setShowModal}>
@@ -26,7 +39,8 @@ const SignInModal: FC<IModal> = ({ showModal, setShowModal, onSignIn }: IModal) 
         onChange={handleTextInputChange}
         placeholder="Starts with S, example: SCHK..."
       />
-      <Button onClick={() => onSignIn(secretKey)} className="continue" $dark>Connect</Button>
+      {error && <ErrorText>{error}</ErrorText>}
+      <Button onClick={handleButtonClick} className="continue" $dark>Connect</Button>
     </BaseModal>
   );
 };
