626282ea70d34e2b31294487938a9e8181311bbe
ozkavosh <augustoslva@hotmail.com>
2023-10-27 11:39:34 -0300

    Update vite.config.ts with missing node dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true, 
+          process: true,
+        }), 
+        NodeModulesPolyfillPlugin() 
+      ]
+    }
+  }, 
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
+      ]
+    }
+  }
 })
