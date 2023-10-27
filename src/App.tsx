ec803c6e0151e7ba5e58464c9e0d6d90bd62b44a
ozkavosh <augustoslva@hotmail.com>
2023-10-27 14:06:53 -0300

    Update App.tsx account context provider and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyles } from "./styles/Global"
-import { theme } from "./styles/Theme"
-import Header from "./components/Header"
-import { Navigation } from "./routes"
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/Global";
+import { theme } from "./styles/Theme";
+import Header from "./components/Header";
+import { Navigation } from "./routes";
+import { AccountContextProvider } from "./context/AccountContext";
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Header />
-      <Navigation />
+      <AccountContextProvider>
+        <Navigation />
+      </AccountContextProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
