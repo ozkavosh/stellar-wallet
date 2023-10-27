993f6e964cda909714c49e0c69665cc5cc835ca4
ozkavosh <augustoslva@hotmail.com>
2023-10-27 14:00:08 -0300

    Add account context reducer

---
+++ src/context/reducers/account.ts
@@ -0,0 +1,45 @@
+import StellarSdk from "stellar-sdk";
+
+export const ACCOUNT_INITIAL_STATE = {
+  publicKey: "",
+  secretKey: "",
+};
+
+export const accountReducer = (
+  state: IAccountState,
+  action: IAccountAction
+): IAccountState => {
+  switch (action.type) {
+    case "SET_SECRET_KEY":
+      return {
+        ...state,
+        secretKey: action.payload as string,
+      };
+    case "SET_PUBLIC_KEY":
+      return {
+        ...state,
+        publicKey: action.payload as string,
+      };
+    case "SET_ACCOUNT":
+      return {
+        ...(action.payload as IAccountState),
+      };
+    case "LOGIN_WITH_SECRET_KEY": {
+      try {
+        const secretKey = action.payload;
+        const account = StellarSdk.Keypair.fromSecret(secretKey);
+
+        return {
+          secretKey: account.secret(),
+          publicKey: account.publicKey(),
+        };
+      } catch {
+        return {
+          ...state,
+        };
+      }
+    }
+    default:
+      return state;
+  }
+};
