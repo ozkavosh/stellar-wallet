5de11043bdc0b020232aa4639dac804195e0b20d
ozkavosh <augustoslva@hotmail.com>
2023-10-30 18:04:23 -0300

    Update App.tsx with Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { theme } from "./styles/Theme";
 import Header from "./components/Header";
 import { Navigation } from "./routes";
 import { AccountContextProvider } from "./context/AccountContext";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <AccountContextProvider>
         <Navigation />
       </AccountContextProvider>
+      <Footer />
     </ThemeProvider>
   );
 }
