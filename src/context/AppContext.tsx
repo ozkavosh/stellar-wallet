7acbe5fcd8dc72520191522ca8d0ace11da818a3
ozkavosh <augustoslva@hotmail.com>
2023-10-31 17:34:53 -0300

    Add AppContext component

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,33 @@
+import { useContext, createContext, useReducer } from "react";
+import { APP_INITIAL_STATE, appReducer } from "./reducers/app";
+
+const AppContext = createContext<IAppContext | null>(null);
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+
+  if (!context)
+    throw new Error("useAppContext must be used within a AppContextProvider");
+
+  return context;
+};
+
+export const AppContextProvider = ({ children }: React.PropsWithChildren) => {
+  const [appState, dispatch] = useReducer(appReducer, APP_INITIAL_STATE);
+
+  const toggleLoading = () => {
+    dispatch({ type: "TOGGLE_LOADING" });
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        appState,
+        dispatch,
+        toggleLoading
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
