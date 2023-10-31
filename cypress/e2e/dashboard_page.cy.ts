524ab6dbc557337f30151f90297adaa292697e16
ozkavosh <augustoslva@hotmail.com>
2023-10-31 13:17:03 -0300

    Update Dashboard tests with logout functionality

--- cypress/e2e/dashboard_page.cy.ts
+++ cypress/e2e/dashboard_page.cy.ts
@@ -1,18 +1,16 @@
 describe("The Dashboard Page", () => {
   it("Should redirect to Home when user is not authenticated", () => {
     cy.visit("/dashboard");
-    cy.location().should(({ pathname }) =>
-      expect(pathname).to.eq("/")
-    );
+    cy.location().should(({ pathname }) => expect(pathname).to.eq("/"));
   });
 
   beforeEach(() => {
-    cy.visit('/');
-    cy.get('button').contains('Connect with a secret key').click();
+    cy.visit("/");
+    cy.get("button").contains("Connect with a secret key").click();
     cy.get("input").type(
-      "SCIB7TEDNENDEVW6AYWYQ7IHSFL5CJ67JK4ACZYX6JHMBIKV74OGDBJ7",
+      "SCIB7TEDNENDEVW6AYWYQ7IHSFL5CJ67JK4ACZYX6JHMBIKV74OGDBJ7"
     );
-    cy.get('button').contains('Connect').click();
+    cy.get("button").contains("Connect").click();
   });
 
   it("Should contain a header and a footer", () => {
@@ -20,7 +18,12 @@ describe("The Dashboard Page", () => {
     cy.get("footer").should("exist");
   });
 
-  it("Should contain two buttons", () => {
-    cy.get("button").should("have.length", 2);
+  it("Should contain three buttons", () => {
+    cy.get("button").should("have.length", 3);
+  });
+
+  it("Should redirect to Home on successful logout", () => {
+    cy.get("button").contains("Sign Out").click();
+    cy.location().should(({ pathname }) => expect(pathname).to.eq("/"));
   });
 });
