2816d2c70ed79af85389bced0cd13f3bdbf8f246
ozkavosh <augustoslva@hotmail.com>
2023-10-31 13:10:51 -0300

    Update Header with logout functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,46 @@
 import { FC } from "react";
-import { Wrapper, Title, Container } from "./style";
+import {
+  Wrapper,
+  Title,
+  Container,
+  PublicKeyContainer,
+  ProfilePicture,
+  AccountContainer,
+  LogoutButton,
+  PublicKey,
+} from "./style";
+import defaultProfilePicture from "../../assets/img/account_profile_picture.png";
+import { useAccountContext } from "../../context/AccountContext";
+import { MdCopyAll } from "react-icons/md";
+import shortenKey from "../../utils/shortenKey";
+import handleCopyButtonClick from "../../utils/handleCopyKeysButton";
+import CopyPopup from "../CopyPopup";
 
 const Header: FC = () => {
+  const {
+    accountState: { publicKey },
+    logout,
+  } = useAccountContext();
+
   return (
     <Wrapper>
-        <Container>
-            <Title>Stellar-Wallet</Title>
-        </Container>
+      <Container>
+        <Title>Stellar-Wallet</Title>
+        {publicKey && (
+          <AccountContainer>
+            <CopyPopup onClick={() => handleCopyButtonClick(publicKey)}>
+              <PublicKeyContainer>
+                <ProfilePicture src={defaultProfilePicture} />
+                <PublicKey>{shortenKey(publicKey)}</PublicKey>
+                <MdCopyAll />
+              </PublicKeyContainer>
+            </CopyPopup>
+            <LogoutButton onClick={logout}>Sign Out</LogoutButton>
+          </AccountContainer>
+        )}
+      </Container>
     </Wrapper>
-  )
-}
+  );
+};
 
 export default Header;
--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -10,6 +10,7 @@ export const Wrapper = styled.header`
 
 export const Container = styled.div`
   display: flex;
+  justify-content: space-between;
   width: 85%;
 `;
 
@@ -17,3 +18,52 @@ export const Title = styled.h1`
   font-size: 2rem;
   color: ${(props) => props.theme.colors.primary};
 `;
+
+export const PublicKeyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: .5rem;
+  font-size: 1.2rem;
+  cursor: pointer;
+
+  &:hover {
+    opacity: .8;
+  }
+
+  svg {
+    color: ${(props) => props.theme.colors.secondary};
+  }
+`;
+
+export const ProfilePicture = styled.img`
+  width: 2rem;
+  height: 2rem;
+  background-color: ${(props) => props.theme.colors.primary};
+  border-radius: 50%;
+`;
+
+export const AccountContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+`;
+
+export const LogoutButton = styled.button`
+  background: none;
+  color: ${(props) => props.theme.colors.secondary};
+  font-size: 1.2rem;
+  padding: .5rem 1rem;
+  border-left: 1px solid ${(props) => props.theme.colors.white};
+  cursor: pointer;
+
+  &:hover {
+    opacity: .8;
+  }
+`;
+
+export const PublicKey = styled.p`
+  font-size: 1.2rem;
+  color: ${(props) => props.theme.colors.primary};
+`; 
