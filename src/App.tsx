047c4713195a3d5a703d5579aaae625856e98994
ozkavosh <augustoslva@hotmail.com>
2023-10-31 17:37:54 -0300

    Update App.tsx with AppContext component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/Global";
-import { theme } from "./styles/Theme";
-import Header from "./components/Header";
 import { Navigation } from "./routes";
 import { AccountContextProvider } from "./context/AccountContext";
+import { AppContextProvider } from "./context/AppContext";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { theme } from "./styles/Theme";
+import Loading from "./components/Loading";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <AccountContextProvider>
-        <Header />
-          <Navigation />
-        <Footer />
-      </AccountContextProvider>
+      <AppContextProvider>
+        <Loading />
+        <AccountContextProvider>
+          <Header />
+            <Navigation />
+          <Footer />
+        </AccountContextProvider>
+      </AppContextProvider>
     </ThemeProvider>
   );
 }
