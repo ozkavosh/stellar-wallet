c8f5b064d4567c0d4f85e2d3c14729c72d0671dd
ozkavosh <augustoslva@hotmail.com>
2023-11-06 10:42:46 -0300

    Update AuthRoute and NoAuthRoute verification prop

--- src/routes/NoAuthRoute/index.tsx
+++ src/routes/NoAuthRoute/index.tsx
@@ -3,13 +3,11 @@ import { useAccountContext } from "../../context/AccountContext";
 import { Navigate } from "react-router";
 
 const NoAuthRoute = ({ children }: React.PropsWithChildren) => {
-  const { accountState } = useAccountContext();
+  const {
+    accountState: { publicKey },
+  } = useAccountContext();
 
-  return !accountState.secretKey ? (
-    <>{children}</>
-  ) : (
-    <Navigate to="/dashboard" />
-  );
+  return !publicKey ? <>{children}</> : <Navigate to="/dashboard" />;
 };
 
 export default NoAuthRoute;
